ae43837b782bdb62bc73c8e083fcf5255e222d07
pallavi <shikhare.pallavi@gmail.com>
2022-03-15 07:52:24 +0530

    First Commit

---
+++ src/Test1.java
@@ -0,0 +1,15 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String hungry = "yes";
+		
+		if(hungry == "yes")
+			System.out.println("eating....");
+		else
+			System.out.println("Do work....");
+	}
+
+}
